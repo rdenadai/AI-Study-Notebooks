7494deddb73ca8a98dca94063a44331e7cb17336
Rodolfo De Nadai <rdenadai@gmail.com>
2019-03-13 16:09:33 -0300

    NN com qualidade próxima a do scikit-learn

--- notebooks/src/nn.py
+++ notebooks/src/nn.py
@@ -42,8 +42,9 @@ class Sigmoid(Layer):
         super().__init__()
 
     def forward(self, Z):
-        return 1. / (1 + np.exp(-Z))
-    
+        K = Z - np.max(Z, axis=1, keepdims=True)
+        return 1. / (1 + np.exp(-K))
+
     def backward(self, Z, grad):
         return Z * (1 - Z)
 
@@ -55,7 +56,7 @@ class Tahn(Layer):
 
     def forward(self, Z):
         return np.tanh(Z)
-    
+
     def backward(self, Z, grad):
         return 1 - (np.tanh(Z)**2)
 
@@ -66,9 +67,9 @@ class Softmax(Layer):
         super().__init__()
 
     def forward(self, Z):
-        y = np.exp(Z - np.max(Z, axis=1, keepdims=True))
-        return y / np.sum(y, axis=1, keepdims=True)
-    
+        K = np.exp(Z - np.max(Z, axis=1, keepdims=True))
+        return K / np.sum(K, axis=1, keepdims=True)
+
     def backward(self, Z, grad):
         return grad
 
@@ -77,11 +78,9 @@ class Dense(Layer):
 
     def __init__(self, inputs=1, outputs=1, activation=ReLU()):
         super().__init__()
-        self.W = np.random.rand(inputs, outputs) * np.sqrt(2 / (outputs + inputs))
-        self.B = np.zeros((1, outputs))
-        self.Z = None
-        self.A = np.zeros((inputs, self.W.shape[0]))
-        
+        self.inputs = inputs
+        self.outputs = outputs
+
         if isinstance(activation, Sigmoid):
             self._activation = activation
         elif isinstance(activation, ReLU):
@@ -91,44 +90,56 @@ class Dense(Layer):
         elif isinstance(activation, Softmax):
             self._activation = activation
 
+    def init(self, name):
+        super().init(name)
+        inp, out = self.inputs, self.outputs
+        self.W = np.random.rand(inp, out) * np.sqrt(2 / (inp + out))
+        self.B = np.zeros((1, out))
+        self.Z = None
+        self.A = np.zeros((inp, self.W.shape[0]))
+
     def forward(self, Z):
         self.Z = Z
         self.A = self._activation.forward(Z.dot(self.W) + self.B)
         return self.A
 
-    def backward(self, Z, delta_in):
-        delta_out = delta_in.dot(self.W.T)
-        act = self._activation.backward(Z, delta_out)
+    def backward(self, A, loss):
+        grad = self._activation.backward(self.A, loss)
         return (
-            np.clip(act, -1, 1),  # grad
-            act.T.dot(delta_in),  # dW 
-            np.sum(delta_in, axis=0)  # dB
+            np.clip(loss.dot(self.W.T), -1, 1),  # dE
+            A.T.dot(grad),  # dW 
+            np.sum(grad, axis=0, keepdims=True)  # dB
         )
 
 
 class NeuralNetwork:
 
-    def __init__(self, layers, X, y, tol=1e-4, loss='mse'):
+    def __init__(self, layers, X, y, epochs=1500, lr=1e-3, batch_size=32, loss='mse', tol=1e-4):
         # Seed the generator - to make sure that we always get the same random numbers.
         # 42 the meaning of life
         np.random.seed(42)
-        
+
         # Naming the layers (indexing)
         self._total_layers = len(layers)
         for k in range(self._total_layers):
             layers[k].init(k)
 
         # Initialization
-        self._layers = layers
         self._X = X
-        self._y = y
-        
-        self._tol = tol
+        self._y = self._one_hot_encode(y)
+        self._classes = self._y.shape[1]
+        self._layers = layers
+
+        # Hyperparameters
+        self._epochs = epochs
+        self._lr = lr
+        self._batch_size = batch_size
         self._loss = self._mse
         if loss == 'cross_entropy':
             self._loss = self._cross_entropy
-        self._y = self._one_hot_encode(self._y)
-        
+        self._tol = tol
+
+        # Internal
         self._total_samples = 1. / self._y.shape[0]
         self._mem_weights = {}
 
@@ -137,26 +148,35 @@ class NeuralNetwork:
 
     def _one_hot_encode(self, y):
         return pd.get_dummies(y).values
-    
+
     def _mse(self, y, Z):
-        return np.sum((Z-y)**2) * self._total_samples
+        loss = Z - y
+        cost = np.sum(loss**2) * self._total_samples
+        return loss, cost
 
     def _cross_entropy(self, y, Z):
-        Z = Z.clip(min=1e-12)
-        return -np.sum(y * np.log(Z)) * self._total_samples
-    
+        loss = Z - y
+        Z = np.clip(Z, 1e-12, 1 - 1e-12)
+        if self._classes == 2:
+            cost = -np.sum((y * np.log(Z)) + ((1 - y) * np.log(1 - Z)))
+        else:
+            cost = -np.sum(y * np.log(Z)) * self._total_samples
+        return loss, cost
+
     def _forward(self, Z):
         for i, layer in enumerate(self._layers):
             Z = layer.forward(Z)
         return Z
 
-    def _backward(self, layer_inputs, grad):
+    def _backward(self, layer_inputs, loss):
         for layer_index in range(self._total_layers)[::-1]:
             layer = self._layers[layer_index]
-            grad, dW, dB = layer.backward(layer_inputs[layer_index], grad)
+            loss, dW, dB = layer.backward(layer_inputs[layer_index], loss)
             self._mem_weights[f'{layer}'] = (dW, dB)
 
-    def _update_weights(self, m, lr):
+    def _update_weights(self):
+        m = 1. / self._batch_size
+        lr = self._lr
         for layer in reversed(self._layers):
             W, B = self._mem_weights[f'{layer}']
             layer.W -= lr * (W * m)
@@ -166,16 +186,15 @@ class NeuralNetwork:
         permutation = np.random.permutation(X.shape[0])
         return X[permutation], y[permutation]
 
-    def train(self, epochs=1500, lr=1e-3, batch_size=32):
+    def train(self):
         error_step = []
         total_expected_error = 0
 
         # Batch size iteration
-        mb = math.ceil(self._X.shape[0] / batch_size)
-        m = 1. / batch_size
+        mb = math.ceil(self._X.shape[0] / self._batch_size)
 
         iter_n = 0
-        for ep, epoch in enumerate(range(epochs)):
+        for ep, epoch in enumerate(range(self._epochs)):
             # Shuffle dataset in each epoch
             X, y = self._shuffle(self._X.copy(), self._y.copy())
 
@@ -184,26 +203,26 @@ class NeuralNetwork:
             k = 0
             for _ in range(mb):
                 # Mini batch crop
-                ini, end = k * batch_size, (k + 1) * batch_size
+                ini, end = k * self._batch_size, (k + 1) * self._batch_size
                 batch_X, batch_y = X[ini:end, :], y[ini:end, :]
                 k += 1
 
                 # Forward
                 Z = self._forward(batch_X)
-                
+
                 # Error
-                delta_out = Z - batch_y
-                
+                loss, cost = self._loss(batch_y, Z)
+
                 # Backward / Backprop
                 layer_inputs = [batch_X] + [layer.A for layer in self._layers]
-                self._backward(layer_inputs, delta_out)
+                self._backward(layer_inputs, loss)
 
                 # Update weights and bias
-                self._update_weights(m, lr)
+                self._update_weights()
 
                 # Loss
-                total_error += self._loss(batch_y, Z)
-            
+                total_error += cost
+
             if np.abs(total_expected_error-total_error) < self._tol:
                 iter_n += 1
             # Early stop, no improvements after 10 iterations
