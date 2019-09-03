d2fcabbfff6fc93e2a058edea813047dda67f103
Rodolfo De Nadai <rdenadai@gmail.com>
2019-09-03 13:53:48 -0300

    working

--- code/qlearning/train.py
+++ code/qlearning/train.py
@@ -2,17 +2,17 @@ import os
 import time
 import pickle
 import numpy as np
-from code.lib.utils import randargmax
+from code.lib.utils import randargmax, normalize
 
 
-EPISODES = 1_500_000
-MAX_STEPS = 1500
+EPISODES = 5_000
+MAX_STEPS = 99
 
 N_ACTIONS = 4
-LEARNING_RATE = 0.01
-GAMMA = 0.999
+LEARNING_RATE = 0.1
+GAMMA = 0.3
 
-DECAY_RATE = 0.0001
+DECAY_RATE = 0.001
 EPSILON = 0.99
 MAX_EPSILON = EPSILON
 MIN_EPSILON = 0.1
@@ -49,25 +49,20 @@ def generate_world():
     F, E = FOOD, ENEMY
     # return np.array(
     #     [
-    #         [0, 0, 0, 0, E, 0, 0, 0, 0, 0],
-    #         [0, E, E, 0, F, E, 0, 0, 0, 0],
-    #         [0, 0, 0, F, 0, 0, 0, E, 0, 0],
-    #         [0, 0, 0, E, 0, E, 0, 0, 0, 0],
-    #         [0, 0, E, E, 0, 0, 0, 0, 0, 0],
-    #         [0, 0, F, 0, E, 0, 0, E, 0, 0],
-    #         [0, E, 0, 0, 0, 0, F, 0, 0, 0],
-    #         [0, 0, 0, 0, 0, 0, 0, E, 0, 0],
-    #         [0, 0, 0, E, 0, 0, 0, 0, 0, 0],
-    #         [0, 0, 0, 0, 0, 0, 0, 0, 0, F],
+    #         [0, 0, 0, 0, F],
+    #         [E, E, E, 0, 0],
+    #         [0, 0, F, 0, 0],
+    #         [E, 0, E, 0, E],
+    #         [0, F, 0, 0, 0],
     #     ]
     # )
-    return np.array([[0, F, 0, 0], [0, E, 0, E], [E, 0, F, 0], [F, 0, E, 0]])
+    return np.array([[0, 0, E], [E, 0, F], [F, 0, 0]])
 
 
 if __name__ == "__main__":
     try:
         zero_world = generate_world()
-        size = len(zero_world[0, :])
+        size = np.max(zero_world.shape)
         # Q-Table
         try:
             with open("code/qlearning/files/qtable.pkl", "rb") as f:
@@ -75,10 +70,11 @@ if __name__ == "__main__":
         except Exception as e:
             extra_state = len(zero_world[zero_world == FOOD])
             Q = np.zeros((size ** 2 + extra_state, N_ACTIONS))
+            # Q = -np.ones((size ** 2 + extra_state, N_ACTIONS))
 
         for episode in range(EPISODES):
             PLAYER_POS = {"x": 0, "y": 0}
-            state, t_reward, reward = 0, 0, 0
+            state, action, t_reward, reward = 0, 0, 0, 0
             win = False
             stop = False
             world = np.copy(zero_world)
@@ -102,16 +98,18 @@ if __name__ == "__main__":
                 # New state
                 new_state = x + (y * size)
 
+                # Clear reward
+                reward = 0
                 # Collision
                 if world[y, x] == FOOD:
-                    reward += 1
+                    reward = 25
                     t_reward += 1
                     new_state = food_position[(y, x)]
                 elif world[y, x] == ENEMY:
-                    reward = -1
+                    reward = -100
                     stop = True
                 else:
-                    reward = -1e-3
+                    reward = -1
 
                 # No food... stop
                 if not np.any(world == FOOD):
@@ -125,14 +123,12 @@ if __name__ == "__main__":
                 world[y, x] = PLAYER
 
                 # Update Q-Table
-                if stop:
-                    Q[state, action] += LEARNING_RATE * (reward - Q[state, action])
+                target = LEARNING_RATE * (reward + GAMMA * np.max(Q[new_state, :]))
+                if win:
+                    Q[state, action] += target
                 else:
-                    target = reward + GAMMA * np.max(Q[new_state, :])
-                    Q[state, action] += LEARNING_RATE * (target - Q[state, action])
+                    Q[state, action] = (1 - LEARNING_RATE) * Q[state, action] + target
 
-                # Clear reward
-                reward = 0
                 # Pass new state
                 state = new_state
 
@@ -142,7 +138,8 @@ if __name__ == "__main__":
             print(f"Episode: {episode}")
             print(f"Reward : {np.round(t_reward, 2)}")
             print(f"Epsilon: {EPSILON}")
-            # time.sleep(0.25)
+            print("-" * 20)
+            time.sleep(0.01)
             os.system("clear")
 
             EPSILON = MIN_EPSILON + (MAX_EPSILON - MIN_EPSILON) * np.exp(
